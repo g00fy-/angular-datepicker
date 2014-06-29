b3649b8e6b59041b2c1159f3ea7d3a649d8efad8
Alex Edwards <edwards.alex@gmail.com>
2014-06-29 13:57:23 -0700

    Added tests for the utils factory

---
+++ test/spec/datePickerUtilsTest.js
@@ -0,0 +1,136 @@
+describe('Test date Picker Utils', function(){
+  var utils, constants;
+
+  var model = new Date('2014-06-29T19:00:00.000Z');
+
+  beforeEach(angular.mock.module('datePicker'));
+
+  beforeEach(angular.mock.inject(function($injector){
+    utils = $injector.get('datePickerUtils');
+    constants = $injector.get('datePickerConfig');
+  }));
+
+  it('get default visible minutes', function(){
+    var mins = utils.getVisibleMinutes(null, constants.step);
+
+    expect(mins).toBeDefined();
+  });
+
+  it('get visible mins provided date', function(){
+    var start = new Date('2014-06-29T19:00:00.000Z');
+    var end = new Date('2014-06-29T19:55:00.000Z');
+    var mins = utils.getVisibleMinutes(start, constants.step);
+
+    expect(mins).toBeDefined();
+    expect(mins[0]).toEqual(start);
+    expect(mins[mins.length-1]).toEqual(end);
+  });
+
+  it('get default visible weeks', function(){
+    var weeks = utils.getVisibleWeeks(null, constants.step);
+
+    expect(weeks).toBeDefined();
+  });
+
+  it('get visible weeks provided date', function(){
+    var start = new Date('2014-05-26T07:00:00.000Z');
+    var end = new Date('2014-07-06T07:00:00.000Z');
+    var chosen = new Date('2014-06-29T19:00:00.000Z');
+    var weeks = utils.getVisibleWeeks(chosen, constants.step);
+
+    expect(weeks).toBeDefined();
+    expect(weeks[0][0]).toEqual(start);
+    expect(weeks[5][weeks[5].length-1]).toEqual(end);
+  });
+
+  it('get default visible years', function(){
+    var years = utils.getVisibleYears(null, constants.step);
+
+    expect(years).toBeDefined();
+  });
+
+  it('get visible years provided date', function(){
+    var start = new Date('2009-01-01T08:00:00.000Z');
+    var end = new Date('2020-01-01T08:00:00.000Z');
+    var chosen = new Date('2014-06-29T19:00:00.000Z');
+    var years = utils.getVisibleYears(chosen, constants.step);
+
+    expect(years).toBeDefined();
+    expect(years[0]).toEqual(start);
+    expect(years[years.length-1]).toEqual(end);
+  });
+
+  it('get default days of week', function(){
+    var days = utils.getDaysOfWeek(null);
+
+    expect(days).toBeDefined();
+  });
+
+  it('get days of week provided date', function(){
+    var start = new Date('2014-05-26T07:00:00.000Z');
+    var end = new Date('2014-06-01T07:00:00.000Z');
+    var days = utils.getDaysOfWeek(start);
+
+    expect(days).toBeDefined();
+    expect(days[0]).toEqual(start);
+    expect(days[days.length-1]).toEqual(end);
+  });
+
+  it('get default months', function(){
+    var months = utils.getVisibleMonths(null);
+
+    expect(months).toBeDefined();
+  });
+
+  it('get default months provided date', function(){
+    var start = new Date('2014-01-01T08:00:00.000Z');
+    var end = new Date('2014-12-01T08:00:00.000Z');
+    var chosen = new Date('2014-06-29T19:00:00.000Z');
+    var months = utils.getVisibleMonths(chosen);
+
+    expect(months).toBeDefined();
+    expect(months[0]).toEqual(start);
+    expect(months[months.length-1]).toEqual(end);
+  });
+
+  it('get default hours', function(){
+    var hours = utils.getVisibleHours(null);
+
+    expect(hours).toBeDefined();
+  });
+
+  it('get default hours provided date', function(){
+    var start = new Date('2014-06-29T07:00:00.000Z');
+    var end = new Date('2014-06-30T06:00:00.000Z');
+    var chosen = new Date('2014-06-29T19:00:00.000Z');
+    var hours = utils.getVisibleHours(chosen);
+
+    expect(hours).toBeDefined();
+    expect(hours[0]).toEqual(start);
+    expect(hours[hours.length-1]).toEqual(end);
+  });
+
+  it('model is after date', function(){
+    var dateAfter =  new Date('2014-06-29T20:00:00.000Z');
+
+    expect(utils.isAfter(model, dateAfter)).toBe(true);
+    expect(utils.isBefore(model, dateAfter)).toBe(false);
+  });
+
+  it('model is before date', function(){
+    var dateAfter =  new Date('2014-06-29T18:00:00.000Z');
+
+    expect(utils.isAfter(model, dateAfter)).toBe(false);
+    expect(utils.isBefore(model, dateAfter)).toBe(true);
+  });
+
+  it('model is almost same', function(){
+    var dateSimilar =  new Date('2014-06-29T19:00:55.555Z');
+
+    expect(utils.isSameYear(model, dateSimilar)).toBe(true);
+    expect(utils.isSameMonth(model, dateSimilar)).toBe(true);
+    expect(utils.isSameDay(model, dateSimilar)).toBe(true);
+    expect(utils.isSameHour(model, dateSimilar)).toBe(true);
+    expect(utils.isSameMinutes(model, dateSimilar)).toBe(true);
+  });
+});
