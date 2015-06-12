b8d905209725453aa9b3841f5e2d40360275ad49
Emanuel Ralha <emaralha@yahoo.com>
2015-06-12 16:26:55 +0100

    Testing for issue #118

--- dist/index.js
+++ dist/index.js
@@ -190,6 +190,13 @@ Module.directive('datePicker', ['datePickerConfig', 'datePickerUtils', function 
           date.setFullYear(date.getFullYear() + delta);
           break;
         case 'date':
+          /* Reverting from ISSUE #113
+          var dt = new Date(date);
+          date.setMonth(date.getMonth() + delta);
+          if (date.getDate() < dt.getDate()) {
+            date.setDate(0);
+          }
+          */
           date.setMonth(date.getMonth() + delta);
           break;
         case 'hours':
