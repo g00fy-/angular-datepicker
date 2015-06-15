3a06651df4ffc10c86d078c1be838270b5cc7269
Romain Gonord <romain.gonord@smile.fr>
2015-06-15 11:48:07 +0200

    update karam conf to match latest version needs

--- karma.conf.js
+++ karma.conf.js
@@ -1,56 +1,61 @@
-// Karma configuration
+module.exports = function (config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath : '',
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // list of files / patterns to load in the browser
+    files : [ 'app/components/angular/angular.js', 'app/components/angular-mocks/angular-mocks.js', 'app/scripts/*.js', 'app/scripts/**/*.js', 'test/mock/**/*.js', 'test/spec/**/*.js' ],
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
-];
+    // list of files to exclude
+    exclude : [
 
-// list of files to exclude
-exclude = [];
+    ],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    preprocessors : {
 
-// web server port
-port = 8080;
+    },
 
-// cli runner port
-runnerPort = 9100;
+    proxies : {
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    },
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters : [ 'progress' ],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // web server port
+    port : 8080,
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+    // enable / disable colors in the output (reporters and logs)
+    colors : true,
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
+    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel : config.LOG_INFO,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    autoWatch : true,
+
+    // frameworks to use
+    frameworks : [ 'jasmine' ],
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers : [ 'PhantomJS' ],
+
+    plugins : [ 'karma-chrome-launcher', 'karma-firefox-launcher', 'karma-script-launcher', 'karma-phantomjs-launcher', 'karma-jasmine' ],
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout : 5000,
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun : false
+  });
+};
