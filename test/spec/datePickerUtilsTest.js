388210814efb9add7751357fad6fb691ebd8ad14
Daniel Talash <dan.talash@gmail.com>
2015-10-28 11:36:04 -0700

    Updated tests, bower config
    * Tests now use moment objects, and moment comparison functions
    * Removed several tests (see comment in tests for explanation)
    * Bower config modified to remove lodash dependency and add moment and
    moment-timezone

--- test/spec/datePickerUtilsTest.js
+++ test/spec/datePickerUtilsTest.js
@@ -1,92 +1,60 @@
 describe('Test date Picker Utils', function(){
-  var utils, constants;
+  var utils, constants, tz = 'UTC';
 
   /**
-   * Getting timezone offset string
-   * @see http://stackoverflow.com/questions/1091372/getting-the-clients-timezone-in-javascript
+   * Creates a moment object from an iso8601 string, using a pre-set timezone.
    */
-  function getTimezoneAsString() {
-    function pad(number, length){
-      var str = "" + number
-      while (str.length < length) {
-        str = '0'+str
-      }
-      return str
-    }
-
-    var offset = new Date().getTimezoneOffset();
-
-    return ((offset < 0 ? '+':'-') + // Note the reversed sign!
-      pad(parseInt(Math.abs(offset/60)), 2) + pad(Math.abs(offset%60), 2));
+  function dateBuilder(iso8601) {
+    return moment.tz(iso8601, tz);
   }
 
-  /**
-   * Building a date from an ISO string with current browser TZ
-   * Not valid for phantomJS (@see https://github.com/ariya/phantomjs/issues/10187)
-   */
-  function dateBuilder(isoStringWithoutTZ) {
-    return new Date(isoStringWithoutTZ + getTimezoneAsString());
-  }
-
-  var model = dateBuilder('2014-06-29T19:00:00.000'); // sunday
+  var model = dateBuilder('2014-06-29T19:00:00+00:00'); // sunday
 
   beforeEach(angular.mock.module('datePicker'));
 
   beforeEach(angular.mock.inject(function($injector){
     utils = $injector.get('datePickerUtils');
     constants = $injector.get('datePickerConfig');
+    utils.setParams(tz);
   }));
 
-  it('get default visible minutes', function(){
-    var mins = utils.getVisibleMinutes(null, constants.step);
-
-    expect(mins).toBeDefined();
-  });
+  /*
+   * At no point do we ever need to get minutes, weeks, years, months, or hours for an unknown date. 
+   *    * The only date building util function which is called without a specific date is getDaysOfWeek.
+   *        * This is called via the update() function in datePicker.js if scope.weekdays is falsy.
+   *    * These tests have therefore been removed.
+   */
 
   it('get visible mins provided date', function(){
-
-    var start = dateBuilder('2014-06-29T19:00:00.000'); // sunday
-    var end = dateBuilder('2014-06-29T19:55:00.000'); // sunday
-    var chosen = dateBuilder('2014-06-29T19:00:00.000'); // sunday
+    var start = dateBuilder('2014-06-29T19:00:00+00:00'); // sunday
+    var end = dateBuilder('2014-06-29T19:55:00+00:00'); // sunday
+    var chosen = dateBuilder('2014-06-29T19:00:00+00:00'); // sunday
     var mins = utils.getVisibleMinutes(chosen, constants.step);
 
     expect(mins).toBeDefined();
-    expect(mins[0]).toEqual(start);
-    expect(mins[mins.length-1]).toEqual(end);
-  });
-
-  it('get default visible weeks', function(){
-    var weeks = utils.getVisibleWeeks(null, constants.step);
-
-    expect(weeks).toBeDefined();
+    expect(start.isSame(mins[0])).toBe(true);
+    expect(end.isSame(mins[mins.length-1])).toBe(true);
   });
 
   it('get visible weeks provided date', function(){
-    var start = dateBuilder('2014-05-26T00:00:00.000'); // monday
-    var end = dateBuilder('2014-07-06T00:00:00.000');  // sunday
-    var chosen = ('2014-06-29T19:00:00.000'); // sunday
+    var start = dateBuilder('2014-05-26T00:00:00+00:00'); // monday
+    var end = dateBuilder('2014-07-06T00:00:00+00:00');  // sunday
+    var chosen = ('2014-06-29T19:00:00+00:00'); // sunday
     var weeks = utils.getVisibleWeeks(chosen, constants.step);
 
     expect(weeks).toBeDefined();
-    expect(weeks[0][0]).toEqual(start);
-    expect(weeks[5][weeks[5].length-1]).toEqual(end);
-  });
-
-  it('get default visible years', function(){
-    var years = utils.getVisibleYears(null, constants.step);
-
-    expect(years).toBeDefined();
+    expect(start.isSame(weeks[0][0])).toBe(true);
+    expect(end.isSame(weeks[5][weeks[5].length - 1])).toBe(true);
   });
 
   it('get visible years provided date', function(){
-    var start = dateBuilder('2009-12-31T00:00:00.000'); // thursday
-    var end = dateBuilder('2020-12-31T00:00:00.000'); // wednesday
-    var chosen = dateBuilder('2014-06-29T19:00:00.000'); // sunday
+    var start = dateBuilder('2010-01-01T00:00:00+00:00'); // friday
+    var end = dateBuilder('2021-01-01T00:00:00+00:00'); // friday
+    var chosen = dateBuilder('2014-06-29T19:00:00+00:00'); // sunday
     var years = utils.getVisibleYears(chosen, constants.step);
-
     expect(years).toBeDefined();
-    expect(years[0]).toEqual(start);
-    expect(years[years.length-1]).toEqual(end);
+    expect(start.isSame(years[0])).toBe(true);
+    expect(end.isSame(years[years.length - 1])).toBe(true);
   });
 
   it('get default days of week', function(){
@@ -96,52 +64,42 @@ describe('Test date Picker Utils', function(){
   });
 
   it('get days of week provided date', function(){
-    var start = dateBuilder('2014-05-26T00:00:00.000'); // monday
-    var end = dateBuilder('2014-06-01T00:00:00.000'); // sunday
+    var start = dateBuilder('2014-05-26T00:00:00+00:00'); // monday
+    var end = dateBuilder('2014-06-01T00:00:00+00:00'); // sunday
     var days = utils.getDaysOfWeek(start);
 
     expect(days).toBeDefined();
-    expect(days[0]).toEqual(start);
-    expect(days[days.length-1]).toEqual(end);
+    expect(start.isSame(days[0])).toBe(true);
+    expect(end.isSame(days[days.length-1])).toBe(true);
   });
 
-  it('get default months', function(){
-    var months = utils.getVisibleMonths(null);
-
-    expect(months).toBeDefined();
-  });
 
   it('get default months provided date', function(){
-    var start = dateBuilder('2014-01-01T00:00:00.000'); // wednesday
-    var end = dateBuilder('2014-12-01T00:00:00.000'); // monday
-    var chosen = dateBuilder('2014-06-29T19:00:00.000'); // sunday
+    var start = dateBuilder('2014-01-01T00:00:00+00:00'); // wednesday
+    var end = dateBuilder('2014-12-01T00:00:00+00:00'); // monday
+    var chosen = dateBuilder('2014-06-29T19:00:00+00:00'); // sunday
     var months = utils.getVisibleMonths(chosen);
 
     expect(months).toBeDefined();
-    expect(months[0]).toEqual(start);
-    expect(months[months.length-1]).toEqual(end);
+    expect(start.isSame(months[0])).toBe(true);
+    expect(end.isSame(months[months.length-1])).toBe(true);
   });
 
-  it('get default hours', function(){
-    var hours = utils.getVisibleHours(null);
-
-    expect(hours).toBeDefined();
-  });
 
   it('get default hours provided date', function(){
-    var start = dateBuilder('2014-06-29T00:00:00.000'); // sunday
-    var end = dateBuilder('2014-06-29T23:00:00.000'); // sunday
-    var chosen = dateBuilder('2014-06-29T19:00:00.000'); // sunday
+    var start = dateBuilder('2014-06-29T00:00:00+00:00'); // sunday
+    var end = dateBuilder('2014-06-29T23:00:00+00:00'); // sunday
+    var chosen = dateBuilder('2014-06-29T19:00:00+00:00'); // sunday
     var hours = utils.getVisibleHours(chosen);
 
     expect(hours).toBeDefined();
-    expect(hours[0]).toEqual(start);
-    expect(hours[hours.length-1]).toEqual(end);
+    expect(start.isSame(hours[0])).toBe(true);
+    expect(end.isSame(hours[hours.length-1])).toBe(true);
   });
 
   it('model is after date', function(){
     // model is 19h, dateAfter is 20h, so model should be before and not after
-    var dateAfter =  dateBuilder('2014-06-29T20:00:00.000'); // sunday
+    var dateAfter =  dateBuilder('2014-06-29T20:00:00+00:00'); // sunday
 
     expect(utils.isAfter(model, dateAfter)).toBe(false);
     expect(utils.isBefore(model, dateAfter)).toBe(true);
@@ -149,7 +107,7 @@ describe('Test date Picker Utils', function(){
 
   it('model is before date', function(){
     // model is 19h, dateAfter is 18h, so model should be after and not before
-    var dateAfter =  dateBuilder('2014-06-29T18:00:00.000'); // sunday
+    var dateAfter =  dateBuilder('2014-06-29T18:00:00+00:00'); // sunday
 
     expect(utils.isAfter(model, dateAfter)).toBe(true);
     expect(utils.isBefore(model, dateAfter)).toBe(false);
