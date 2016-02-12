5a579c5f2a90b7ebcdb1f93fea229f937cf6a75d
lcarnevale <luca.carnevale@objectway.it>
2016-02-12 16:56:42 +0100

    Add amd support

--- app/scripts/datePickerUtils.js
+++ app/scripts/datePickerUtils.js
@@ -1,231 +1,237 @@
-/* global moment */
-
-angular.module('datePicker').factory('datePickerUtils', function () {
+(function (global, factory) {
   'use strict';
-  var tz, firstDay;
-  var createNewDate = function (year, month, day, hour, minute) {
-    var utc = Date.UTC(year | 0, month | 0, day | 0, hour | 0, minute | 0);
-    return tz ? moment.tz(utc, tz) : moment(utc);
-  };
+  var fnc;
+  fnc = (typeof exports === 'object' && typeof module !== 'undefined') ? module.exports = factory(require('angular'), require('moment')) :
+    (typeof define === 'function' && define.amd) ? define(['angular', 'moment'], factory) :
+      factory(global.angular, global.moment);
+}(this, function (angular, moment) {
+  'use strict';
+  angular.module('datePicker').factory('datePickerUtils', function () {
+    var tz, firstDay;
+    var createNewDate = function (year, month, day, hour, minute) {
+      var utc = Date.UTC(year | 0, month | 0, day | 0, hour | 0, minute | 0);
+      return tz ? moment.tz(utc, tz) : moment(utc);
+    };
 
-  return {
-    getVisibleMinutes: function (m, step) {
-      var year = m.year(),
-        month = m.month(),
-        day = m.date(),
-        hour = m.hours(), pushedDate,
-        offset = m.utcOffset() / 60,
-        minutes = [], minute;
+    return {
+      getVisibleMinutes: function (m, step) {
+        var year = m.year(),
+          month = m.month(),
+          day = m.date(),
+          hour = m.hours(), pushedDate,
+          offset = m.utcOffset() / 60,
+          minutes = [], minute;
 
-      for (minute = 0 ; minute < 60 ; minute += step) {
-        pushedDate = createNewDate(year, month, day, hour - offset, minute);
-        minutes.push(pushedDate);
-      }
-      return minutes;
-    },
-    getVisibleWeeks: function (m) {
-      m = moment(m);
-      var startYear = m.year(),
+        for (minute = 0; minute < 60; minute += step) {
+          pushedDate = createNewDate(year, month, day, hour - offset, minute);
+          minutes.push(pushedDate);
+        }
+        return minutes;
+      },
+      getVisibleWeeks: function (m) {
+        m = moment(m);
+        var startYear = m.year(),
           startMonth = m.month();
 
-      //Set date to the first day of the month
-      m.date(1);
+        //Set date to the first day of the month
+        m.date(1);
 
-      //Grab day of the week
-      var day = m.day();
+        //Grab day of the week
+        var day = m.day();
 
-      //Go back the required number of days to arrive at the previous week start
-      m.date(firstDay - (day + (firstDay >= day ? 6 : -1)));
+        //Go back the required number of days to arrive at the previous week start
+        m.date(firstDay - (day + (firstDay >= day ? 6 : -1)));
 
-      var weeks = [];
+        var weeks = [];
 
-      while (weeks.length < 6) {
-        if (m.year() === startYear && m.month() > startMonth) {
-          break;
+        while (weeks.length < 6) {
+          if (m.year() === startYear && m.month() > startMonth) {
+            break;
+          }
+          weeks.push(this.getDaysOfWeek(m));
+          m.add(7, 'd');
         }
-        weeks.push(this.getDaysOfWeek(m));
-        m.add(7, 'd');
-      }
-      return weeks;
-    },
-    getVisibleYears: function (d) {
-      var m = moment(d),
+        return weeks;
+      },
+      getVisibleYears: function (d) {
+        var m = moment(d),
+          year = m.year();
+
+        m.year(year - (year % 10));
         year = m.year();
 
-      m.year(year - (year % 10));
-      year = m.year();
+        var offset = m.utcOffset() / 60,
+          years = [],
+          pushedDate,
+          actualOffset;
 
-      var offset = m.utcOffset() / 60,
-        years = [],
-        pushedDate,
-        actualOffset;
-
-      for (var i = 0; i < 12; i++) {
-        pushedDate = createNewDate(year, 0, 1, 0 - offset);
-        actualOffset = pushedDate.utcOffset() / 60;
-        if (actualOffset !== offset) {
-          pushedDate = createNewDate(year, 0, 1, 0 - actualOffset);
-          offset = actualOffset;
+        for (var i = 0; i < 12; i++) {
+          pushedDate = createNewDate(year, 0, 1, 0 - offset);
+          actualOffset = pushedDate.utcOffset() / 60;
+          if (actualOffset !== offset) {
+            pushedDate = createNewDate(year, 0, 1, 0 - actualOffset);
+            offset = actualOffset;
+          }
+          years.push(pushedDate);
+          year++;
         }
-        years.push(pushedDate);
-        year++;
-      }
-      return years;
-    },
-    getDaysOfWeek: function (m) {
-      m = m ? m : (tz ? moment.tz(tz).day(firstDay) : moment().day(firstDay));
+        return years;
+      },
+      getDaysOfWeek: function (m) {
+        m = m ? m : (tz ? moment.tz(tz).day(firstDay) : moment().day(firstDay));
 
-      var year = m.year(),
-        month = m.month(),
-        day = m.date(),
-        days = [],
-        pushedDate,
-        offset = m.utcOffset() / 60,
-        actualOffset;
+        var year = m.year(),
+          month = m.month(),
+          day = m.date(),
+          days = [],
+          pushedDate,
+          offset = m.utcOffset() / 60,
+          actualOffset;
 
-      for (var i = 0; i < 7; i++) {
-        pushedDate = createNewDate(year, month, day, 0 - offset, 0, false);
-        actualOffset = pushedDate.utcOffset() / 60;
-        if (actualOffset !== offset) {
-          pushedDate = createNewDate(year, month, day, 0 - actualOffset, 0, false);
+        for (var i = 0; i < 7; i++) {
+          pushedDate = createNewDate(year, month, day, 0 - offset, 0, false);
+          actualOffset = pushedDate.utcOffset() / 60;
+          if (actualOffset !== offset) {
+            pushedDate = createNewDate(year, month, day, 0 - actualOffset, 0, false);
+          }
+          days.push(pushedDate);
+          day++;
         }
-        days.push(pushedDate);
-        day++;
-      }
-      return days;
-    },
-    getVisibleMonths: function (m) {
-      var year = m.year(),
-        offset = m.utcOffset() / 60,
-        months = [],
-        pushedDate,
-        actualOffset;
+        return days;
+      },
+      getVisibleMonths: function (m) {
+        var year = m.year(),
+          offset = m.utcOffset() / 60,
+          months = [],
+          pushedDate,
+          actualOffset;
 
-      for (var month = 0; month < 12; month++) {
-        pushedDate = createNewDate(year, month, 1, 0 - offset, 0, false);
-        actualOffset = pushedDate.utcOffset() / 60;
-        if (actualOffset !== offset) {
-          pushedDate = createNewDate(year, month, 1, 0 - actualOffset, 0, false);
+        for (var month = 0; month < 12; month++) {
+          pushedDate = createNewDate(year, month, 1, 0 - offset, 0, false);
+          actualOffset = pushedDate.utcOffset() / 60;
+          if (actualOffset !== offset) {
+            pushedDate = createNewDate(year, month, 1, 0 - actualOffset, 0, false);
+          }
+          months.push(pushedDate);
         }
-        months.push(pushedDate);
-      }
-      return months;
-    },
-    getVisibleHours: function (m) {
-      var year = m.year(),
-        month = m.month(),
-        day = m.date(),
-        hours = [],
-        hour, pushedDate, actualOffset,
-        offset = m.utcOffset() / 60;
+        return months;
+      },
+      getVisibleHours: function (m) {
+        var year = m.year(),
+          month = m.month(),
+          day = m.date(),
+          hours = [],
+          hour, pushedDate, actualOffset,
+          offset = m.utcOffset() / 60;
 
-      for (hour = 0 ; hour < 24 ; hour++) {
-        pushedDate = createNewDate(year, month, day, hour - offset, 0, false);
-        actualOffset = pushedDate.utcOffset() / 60;
-        if (actualOffset !== offset) {
-          pushedDate = createNewDate(year, month, day, hour - actualOffset, 0, false);
+        for (hour = 0; hour < 24; hour++) {
+          pushedDate = createNewDate(year, month, day, hour - offset, 0, false);
+          actualOffset = pushedDate.utcOffset() / 60;
+          if (actualOffset !== offset) {
+            pushedDate = createNewDate(year, month, day, hour - actualOffset, 0, false);
+          }
+          hours.push(pushedDate);
         }
-        hours.push(pushedDate);
-      }
 
-      return hours;
-    },
-    isAfter: function (model, date) {
-      return model && model.unix() >= date.unix();
-    },
-    isBefore: function (model, date) {
-      return model.unix() <= date.unix();
-    },
-    isSameYear: function (model, date) {
-      return model && model.year() === date.year();
-    },
-    isSameMonth: function (model, date) {
-      return this.isSameYear(model, date) && model.month() === date.month();
-    },
-    isSameDay: function (model, date) {
-      return this.isSameMonth(model, date) && model.date() === date.date();
-    },
-    isSameHour: function (model, date) {
-      return this.isSameDay(model, date) && model.hours() === date.hours();
-    },
-    isSameMinutes: function (model, date) {
-      return this.isSameHour(model, date) && model.minutes() === date.minutes();
-    },
-    setParams: function (zone, fd) {
-      tz = zone;
-      firstDay = fd;
-    },
-    scopeSearch: function (scope, name, comparisonFn) {
-      var parentScope = scope,
+        return hours;
+      },
+      isAfter: function (model, date) {
+        return model && model.unix() >= date.unix();
+      },
+      isBefore: function (model, date) {
+        return model.unix() <= date.unix();
+      },
+      isSameYear: function (model, date) {
+        return model && model.year() === date.year();
+      },
+      isSameMonth: function (model, date) {
+        return this.isSameYear(model, date) && model.month() === date.month();
+      },
+      isSameDay: function (model, date) {
+        return this.isSameMonth(model, date) && model.date() === date.date();
+      },
+      isSameHour: function (model, date) {
+        return this.isSameDay(model, date) && model.hours() === date.hours();
+      },
+      isSameMinutes: function (model, date) {
+        return this.isSameHour(model, date) && model.minutes() === date.minutes();
+      },
+      setParams: function (zone, fd) {
+        tz = zone;
+        firstDay = fd;
+      },
+      scopeSearch: function (scope, name, comparisonFn) {
+        var parentScope = scope,
           nameArray = name.split('.'),
           target, i, j = nameArray.length;
 
-      do {
-        target = parentScope = parentScope.$parent;
+        do {
+          target = parentScope = parentScope.$parent;
 
-        //Loop through provided names.
-        for (i = 0; i < j; i++) {
-          target = target[nameArray[i]];
-          if (!target) {
-            continue;
+          //Loop through provided names.
+          for (i = 0; i < j; i++) {
+            target = target[nameArray[i]];
+            if (!target) {
+              continue;
+            }
+          }
+
+          //If we reached the end of the list for this scope,
+          //and something was found, trigger the comparison
+          //function. If the comparison function is happy, return
+          //found result. Otherwise, continue to the next parent scope
+          if (target && comparisonFn(target)) {
+            return target;
+          }
+
+        } while (parentScope.$parent);
+
+        return false;
+      },
+      findFunction: function (scope, name) {
+        //Search scope ancestors for a matching function.
+        return this.scopeSearch(scope, name, function (target) {
+          //Property must also be a function
+          return angular.isFunction(target);
+        });
+      },
+      findParam: function (scope, name) {
+        //Search scope ancestors for a matching parameter.
+        return this.scopeSearch(scope, name, function () {
+          //As long as the property exists, we're good
+          return true;
+        });
+      },
+      createMoment: function (m) {
+        if (tz) {
+          return moment.tz(m, tz);
+        } else {
+          //If input is a moment, and we have no TZ info, we need to remove TZ
+          //info from the moment, otherwise the newly created moment will take
+          //the timezone of the input moment. The easiest way to do that is to
+          //take the unix timestamp, and use that to create a new moment.
+          //The new moment will use the local timezone of the user machine.
+          return moment.isMoment(m) ? moment.unix(m.unix()) : moment(m);
+        }
+      },
+      getDate: function (scope, attrs, name) {
+        var result = false;
+        if (attrs[name]) {
+          result = this.createMoment(attrs[name]);
+          if (!result.isValid()) {
+            result = this.findParam(scope, attrs[name]);
+            if (result) {
+              result = this.createMoment(result);
+            }
           }
         }
 
-        //If we reached the end of the list for this scope,
-        //and something was found, trigger the comparison
-        //function. If the comparison function is happy, return
-        //found result. Otherwise, continue to the next parent scope
-        if (target && comparisonFn(target)) {
-          return target;
-        }
-
-      } while (parentScope.$parent);
-
-      return false;
-    },
-    findFunction: function (scope, name) {
-      //Search scope ancestors for a matching function.
-      return this.scopeSearch(scope, name, function(target) {
-        //Property must also be a function
-        return angular.isFunction(target);
-      });
-    },
-    findParam: function (scope, name) {
-      //Search scope ancestors for a matching parameter.
-      return this.scopeSearch(scope, name, function() {
-        //As long as the property exists, we're good
-        return true;
-      });
-    },
-    createMoment: function (m) {
-      if (tz) {
-        return moment.tz(m, tz);
-      } else {
-        //If input is a moment, and we have no TZ info, we need to remove TZ 
-        //info from the moment, otherwise the newly created moment will take 
-        //the timezone of the input moment. The easiest way to do that is to
-        //take the unix timestamp, and use that to create a new moment.
-        //The new moment will use the local timezone of the user machine.
-        return moment.isMoment(m) ? moment.unix(m.unix()) : moment(m);
+        return result;
+      },
+      eventIsForPicker: function (targetIDs, pickerID) {
+        //Checks if an event targeted at a specific picker, via either a string name, or an array of strings.
+        return (angular.isArray(targetIDs) && targetIDs.indexOf(pickerID) > -1 || targetIDs === pickerID);
       }
-    },
-    getDate: function (scope, attrs, name) {
-      var result = false;
-      if (attrs[name]) {
-        result = this.createMoment(attrs[name]);
-        if (!result.isValid()) {
-          result = this.findParam(scope, attrs[name]);
-          if (result) {
-            result = this.createMoment(result);
-          }
-        }
-      }
-
-      return result;
-    },
-    eventIsForPicker: function (targetIDs, pickerID) {
-      //Checks if an event targeted at a specific picker, via either a string name, or an array of strings.
-      return (angular.isArray(targetIDs) && targetIDs.indexOf(pickerID) > -1 || targetIDs === pickerID);
-    }
-  };
-});
+    };
+  });
+}));
--- app/scripts/dateRange.js
+++ app/scripts/dateRange.js
@@ -1,86 +1,92 @@
-/* global moment */
-'use strict';
+(function (global, factory) {
+  'use strict';
+  var fnc;
+  fnc = (typeof exports === 'object' && typeof module !== 'undefined') ? module.exports = factory(require('angular'), require('moment')) :
+    (typeof define === 'function' && define.amd) ? define(['angular', 'moment'], factory) :
+      factory(global.angular, global.moment);
+}(this, function (angular, moment) {
+  'use strict';
+  var Module = angular.module('datePicker');
 
-var Module = angular.module('datePicker');
+  Module.directive('dateRange', ['$compile', 'datePickerUtils', 'dateTimeConfig', function ($compile, datePickerUtils, dateTimeConfig) {
+    function getTemplate(attrs, id, model, min, max) {
+      return dateTimeConfig.template(angular.extend(attrs, {
+        ngModel: model,
+        minDate: min && moment.isMoment(min) ? min.format() : false,
+        maxDate: max && moment.isMoment(max) ? max.format() : false
+      }), id);
+    }
 
-Module.directive('dateRange', ['$compile', 'datePickerUtils', 'dateTimeConfig', function ($compile, datePickerUtils, dateTimeConfig) {
-  function getTemplate(attrs, id, model, min, max) {
-    return dateTimeConfig.template(angular.extend(attrs, {
-      ngModel: model,
-      minDate: min && moment.isMoment(min) ? min.format() : false,
-      maxDate: max && moment.isMoment(max) ? max.format() : false
-    }), id);
-  }
+    function randomName() {
+      return 'picker' + Math.random().toString().substr(2);
+    }
 
-  function randomName() {
-    return 'picker' + Math.random().toString().substr(2);
-  }
-
-  return {
-    scope: {
-      start: '=',
-      end: '='
-    },
-    link: function (scope, element, attrs) {
-      var dateChange = null,
+    return {
+      scope: {
+        start: '=',
+        end: '='
+      },
+      link: function (scope, element, attrs) {
+        var dateChange = null,
           pickerRangeID = element[0].id,
           pickerIDs = [randomName(), randomName()],
           createMoment = datePickerUtils.createMoment,
           eventIsForPicker = datePickerUtils.eventIsForPicker;
 
-      scope.dateChange = function (modelName, newDate) {
-        //Notify user if callback exists.
-        if (dateChange) {
-          dateChange(modelName, newDate);
-        }
-      };
-
-      function setMax(date) {
-        scope.$broadcast('pickerUpdate', pickerIDs[0], {
-          maxDate: date
-        });
-      }
-
-      function setMin(date) {
-        scope.$broadcast('pickerUpdate', pickerIDs[1], {
-          minDate: date
-        });
-      }
-
-      if (pickerRangeID) {
-        scope.$on('pickerUpdate', function (event, targetIDs, data) {
-          if (eventIsForPicker(targetIDs, pickerRangeID)) {
-            //If we received an update event, dispatch it to the inner pickers using their IDs.
-            scope.$broadcast('pickerUpdate', pickerIDs, data);
+        scope.dateChange = function (modelName, newDate) {
+          //Notify user if callback exists.
+          if (dateChange) {
+            dateChange(modelName, newDate);
           }
+        };
+
+        function setMax(date) {
+          scope.$broadcast('pickerUpdate', pickerIDs[0], {
+            maxDate: date
+          });
+        }
+
+        function setMin(date) {
+          scope.$broadcast('pickerUpdate', pickerIDs[1], {
+            minDate: date
+          });
+        }
+
+        if (pickerRangeID) {
+          scope.$on('pickerUpdate', function (event, targetIDs, data) {
+            if (eventIsForPicker(targetIDs, pickerRangeID)) {
+              //If we received an update event, dispatch it to the inner pickers using their IDs.
+              scope.$broadcast('pickerUpdate', pickerIDs, data);
+            }
+          });
+        }
+
+        datePickerUtils.setParams(attrs.timezone);
+
+        scope.start = createMoment(scope.start);
+        scope.end = createMoment(scope.end);
+
+        scope.$watchGroup(['start', 'end'], function (dates) {
+          //Scope data changed, update picker min/max
+          setMin(dates[0]);
+          setMax(dates[1]);
         });
+
+        if (angular.isDefined(attrs.dateChange)) {
+          dateChange = datePickerUtils.findFunction(scope, attrs.dateChange);
+        }
+
+        attrs.onSetDate = 'dateChange';
+
+        var template = '<div><table class="date-range"><tr><td valign="top">' +
+          getTemplate(attrs, pickerIDs[0], 'start', false, scope.end) +
+          '</td><td valign="top">' +
+          getTemplate(attrs, pickerIDs[1], 'end', scope.start, false) +
+          '</td></tr></table></div>';
+
+        var picker = $compile(template)(scope);
+        element.append(picker);
       }
-
-      datePickerUtils.setParams(attrs.timezone);
-
-      scope.start = createMoment(scope.start);
-      scope.end = createMoment(scope.end);
-
-      scope.$watchGroup(['start', 'end'], function (dates) {
-        //Scope data changed, update picker min/max
-        setMin(dates[0]);
-        setMax(dates[1]);
-      });
-
-      if (angular.isDefined(attrs.dateChange)) {
-        dateChange = datePickerUtils.findFunction(scope, attrs.dateChange);
-      }
-
-      attrs.onSetDate = 'dateChange';
-
-      var template = '<div><table class="date-range"><tr><td valign="top">' +
-                    getTemplate(attrs, pickerIDs[0], 'start', false, scope.end) +
-                    '</td><td valign="top">' +
-                    getTemplate(attrs, pickerIDs[1], 'end', scope.start, false) +
-                  '</td></tr></table></div>';
-
-      var picker = $compile(template)(scope);
-      element.append(picker);
-    }
-  };
-}]);
+    };
+  }]);
+}));
